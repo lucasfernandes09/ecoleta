ef051d58472522d2ffcc8e363b3e2fa9ee6628e7
lucasfernandes09 <55118141+lucasfernandes09@users.noreply.github.com>
2020-07-31 18:25:14 -0300

    adding screenShots

---
+++ app/src/main/java/com/app/ecoleta/service/RetrofitService.java
@@ -0,0 +1,26 @@
+package com.app.ecoleta.service;
+
+import com.app.ecoleta.model.Cidades;
+import com.app.ecoleta.model.Estados;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface RetrofitService {
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://servicodados.ibge.gov.br/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    @GET("api/v1/localidades/estados")
+    Call<List<Estados>> recuperarEstados();
+
+    @GET("api/v1/localidades/estados/{UF}/municipios")
+    Call<List<Cidades>> recuperarCidades(@Path("UF") int id_uf);
+}
